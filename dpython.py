8ecabee917d33f25f4f1ec12c91eebff318e3d29
phitazero <kkeke8080@gmail.com>
2025-06-06 20:15:02 +0300

    added appending to files and clearing files

--- dpython.py
+++ dpython.py
@@ -10,7 +10,9 @@ prefix = "_ϕナ"
 PRELUDE = f"import json as {prefix}json"
 
 PRINT_TEMPLATE = f"{{0}}print({prefix}json.dumps({{1}}, indent=4));print(\"\\n\")"
-DUMP_TEMPLATE = f"{{0}}{prefix}json.dump({{1}}, open(\"{{2}}.json\", \"w\"), indent=4)"
+WRITE_TEMPLATE = f"{{0}}{prefix}json.dump({{1}}, open(\"{{3}}.json\", \"w\"), indent=4)"
+APPEND_TEMPLATE = f"{{0}}open(\"{{3}}.json\", \"a\").write({prefix}json.dumps({{1}}, indent=4) + \"\\n\\n\")"
+CLEAR_TEMPLATE = f"{{0}}open(\"{{3}}.json\", \"w\")"
 
 try: codeLines = open(sys.argv[1], "r").readlines()
 except FileNotFoundError:
@@ -24,12 +26,19 @@ except IndexError:
 codeLines = [line.rstrip("\n") for line in codeLines]
 
 def repl(m):
-	if m.group(3) == "":
-		return PRINT_TEMPLATE.format(*m.groups())
+	var = m.group(2)
+	op = m.group(3)
+	dest = m.group(4)
+	if var == "":
+		return CLEAR_TEMPLATE.format(*m.groups())
+	elif op == "=>" and dest != "":
+		return WRITE_TEMPLATE.format(*m.groups())
+	elif op == "=>>" and dest != "":
+		return APPEND_TEMPLATE.format(*m.groups())
 	else:
-		return DUMP_TEMPLATE.format(*m.groups())
+		return PRINT_TEMPLATE.format(*m.groups())
 
-reParts = ("^(\\s*)" "#", "([^#]*?)", "=>", "([A-Za-z0-9_]*)", "$")
+reParts = ("^(\\s*)" "#", "([^#]*?)", "(=>|=>>)", "([A-Za-z0-9_]*)", "$")
 r = re.compile("\\s*".join(reParts))
 
 formattedCodeLines = [r.sub(repl, line) for line in codeLines]
